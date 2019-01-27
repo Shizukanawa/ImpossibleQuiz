2b9cbfa909c199a17c316659ddbadaff3e0c3167
Shizukanawa <deviousfinest@gmail.com>
2019-01-27 03:05:46 +0100

    Added gameover screen and question four

--- ImpossibleQuiz/ImpossibleQuiz.ino
+++ ImpossibleQuiz/ImpossibleQuiz.ino
@@ -72,58 +72,29 @@ void setup() {
 void loop() {
   checkAnswers();
   
-  TSPoint p = ts.getPoint();
-  pinMode(YP, OUTPUT);
-  pinMode(XM, OUTPUT);
-  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
-    p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
-    p.y = map(p.y, TS_MAXY, TS_MINY, tft.height(), 0);
-    Serial.print(p.x);
-    Serial.print(" ");
-    Serial.println(p.y);
-    switch(questionNumber)
-    {
-      case 0:
-        if (p.x >= 26 && p.x < 212)
-        {
-          if(p.y >= 200 && p.y < 260)
-          {
-            questionOne();
-            delay(250);
-            questionNumber = 1;
-          }
-        }
-        break;
-      default:
-        if (p.x >= 10 && p.x < 110) //First block
-        {
-          if (p.y >= 180 && p.y < 240)
-            answer1 = 1;
-          else if (p.y >= 250 && p.y < 300) //Third Block
-            answer3 = 1;
-        }
-        else if (p.x >= 135 && p.x < 230) //Second block
-        {
-          if (p.y >= 180 && p.y < 240)
-            answer2 = 1;
-          else if (p.y >= 250 && p.y < 300) //Fourth Block
-            answer4 = 1;
-        }
-        break;
-    }
-  }
+  checkPresses();
 }
 
 void gameOver(void)
 {
-  tft.fillScreen(WHITE);
+  tft.fillScreen(BLACK);
   answer1 = 0;
   answer2 = 0;
   answer3 = 0;
   answer4 = 0;
   lives = 3;
-  questionNumber = 0;
-  gameStart();
+  questionNumber = -1;
+  tft.setCursor(30, 20);
+  tft.setTextSize(8);
+  tft.setTextColor(RED);
+  tft.println("GAME");
+  tft.setCursor(30, 85);
+  tft.println("OVER");
+
+  tft.setCursor(30, 200);
+  tft.setTextSize(3);
+  tft.setTextColor(GREEN);
+  tft.println("TRY AGAIN?");
 }
 
 void gameStart(void)
@@ -151,6 +122,58 @@ void gameStart(void)
   tft.setTextColor(BLACK);
   tft.setTextSize(4);
   tft.println("START");
+}
+
+void checkPresses(void)
+{
+  TSPoint p = ts.getPoint();
+  pinMode(YP, OUTPUT);
+  pinMode(XM, OUTPUT);
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+    p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+    p.y = map(p.y, TS_MAXY, TS_MINY, tft.height(), 0);
+    Serial.print(p.x);
+    Serial.print(" ");
+    Serial.println(p.y);
+    switch(questionNumber)
+    {
+      case -1:
+        if (p.x >= 30 && p.x < 210)
+        {
+          if(p.y >= 210 && p.y < 230)
+          {
+            questionOne();
+            delay(250);
+            questionNumber = 1;
+          }
+        }
+        break;
+      case 0:
+        if (p.x >= 26 && p.x < 212)
+        {
+          if (p.y >= 200 && p.y < 260)
+          {
+            questionOne();
+            delay(250);
+            questionNumber = 1;
+          }
+        }
+        break;
+      case 4:
+        if (p.x >= 80 && p.x < 200 && p.y >= 35 && p.y < 45)
+        {
+          tft.fillScreen(BLACK);
+          delay(250);
+          ++questionNumber;
+        }
+        else
+          defaultAnswers(p);
+        break;
+      default:
+        defaultAnswers(p);
+        break;
+    }
+  }
 }
 
 void checkAnswers(void)
@@ -183,10 +206,21 @@ void checkAnswers(void)
         }
         break;
       case 3:
-        tft.fillScreen(BLACK);
+        questionFour();
         delay(250);
         answer1 = 0;
         ++questionNumber;
+        break;
+      case 4:
+        --lives;
+        if (lives == 0)
+          gameOver();
+        else
+        {
+          questionFour();
+          delay(250);
+          answer1 = 0;
+        }
         break;
     }
   }
@@ -229,6 +263,17 @@ void checkAnswers(void)
           answer2 = 0;
         }
         break;
+      case 4:
+        --lives;
+        if (lives == 0)
+          gameOver();
+        else
+        {
+          questionFour();
+          delay(250);
+          answer2 = 0;
+        }
+        break;
     }
   }
 
@@ -261,6 +306,17 @@ void checkAnswers(void)
         else
         {
           questionThree();
+          delay(250);
+          answer3 = 0;
+        }
+        break;
+      case 4:
+        --lives;
+        if (lives == 0)
+          gameOver();
+        else
+        {
+          questionFour();
           delay(250);
           answer3 = 0;
         }
@@ -300,7 +356,37 @@ void checkAnswers(void)
           delay(250);
           answer4 = 0;
         }
+        break;
+      case 4:
+        --lives;
+        if (lives == 0)
+          gameOver();
+        else
+        {
+          questionFour();
+          delay(250);
+          answer4 = 0;
+        }
+        break;
     }
+  }
+}
+
+void defaultAnswers(TSPoint p)
+{
+  if (p.x >= 10 && p.x < 110) //First block
+  {
+    if (p.y >= 180 && p.y < 240)
+      answer1 = 1;
+    else if (p.y >= 250 && p.y < 300) //Third Block
+      answer3 = 1;
+  }
+  else if (p.x >= 135 && p.x < 230) //Second block
+  {
+    if (p.y >= 180 && p.y < 240)
+      answer2 = 1;
+    else if (p.y >= 250 && p.y < 300) //Fourth Block
+      answer4 = 1;
   }
 }
 
@@ -491,4 +577,63 @@ void questionThree(void)
   tft.println("TENNIS");
   tft.setCursor(165, 268);
   tft.println("ELBOW");
+}
+
+void questionFour(void)
+{
+  //Reset screen
+  tft.fillScreen(WHITE);
+  
+  //Question number
+  tft.fillRect(0, 0, 50, 50, RED);
+  tft.fillRect(5, 5, 40, 40, WHITE);
+  tft.setCursor(17, 17);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(2);
+  tft.println("4.");
+
+  //Lives
+  tft.setCursor(10, 300);
+  tft.println("LIVES:"); tft.setCursor(90, 300);
+  if (lives == 3)
+    tft.setTextColor(GREEN);
+  else if (lives == 2)
+    tft.setTextColor(YELLOW);
+  else if (lives == 1)
+    tft.setTextColor(RED);
+  tft.println(lives);
+
+  //Question
+  tft.setCursor(110, 17);
+  tft.setTextColor(BLUE);
+  tft.println("CLICK");
+  tft.setCursor(80, 35);
+  tft.println("THE ANSWER");
+
+  //Answer blocks
+  tft.setTextColor(RED);
+  tft.setTextSize(1.5);
+  //1
+  tft.fillRect(10, 180, 100, 50, BLUE);
+  tft.fillRect(20, 190, 80, 30, WHITE);
+  tft.setCursor(25, 202);
+  tft.println("OUT OF ORDER");
+  
+  //2
+  tft.fillRect(130, 180, 100, 50, BLUE);
+  tft.fillRect(140, 190, 80, 30, WHITE);
+  tft.setCursor(145, 202);
+  tft.println("OUT OF ORDER");
+
+  //3
+  tft.fillRect(10, 240, 100, 50, BLUE);
+  tft.fillRect(20, 250, 80, 30, WHITE);
+  tft.setCursor(25, 262);
+  tft.println("OUT OF ORDER");
+
+  //4
+  tft.fillRect(130, 240, 100, 50, BLUE);
+  tft.fillRect(140, 250, 80, 30, WHITE);
+  tft.setCursor(145, 262);
+  tft.println("OUT OF ORDER");
 }
